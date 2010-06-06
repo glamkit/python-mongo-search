bfa8c4b276991dfe3bb8b49cfd72df2b1744f84a
howthebodyworks <git@email.possumpalace.org>
2010-06-05 20:03:37 -0600

    my copy of pymongo can raise ConnectionFailure instead of AutoReconnect

--- mongosearch/tests/test.py
+++ mongosearch/tests/test.py
@@ -25,7 +25,7 @@ def setup_module():
     Instantiate a new mongo daemon and corresponding connection and 
     then insert the appropriate test fixture.
     """
-    from pymongo.connection import AutoReconnect
+    from pymongo.errors import AutoReconnect, ConnectionFailure
     import time
     daemon = _setup_daemon()
     conn_tries = 0
@@ -33,7 +33,7 @@ def setup_module():
         try:
             _connection = util.get_connection(**_settings)
             break
-        except AutoReconnect:
+        except (AutoReconnect, ConnectionFailure):
             conn_tries += 1 # sometimes the daemon doesn't set up straight away
             if conn_tries > 5: 
                 raise #but if we've waited 5 secs, let's give up
